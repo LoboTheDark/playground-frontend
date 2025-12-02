0cc445de87e676534bcaab404a23200ad9d37fab
Lobo <andreas.dahm@gmail.com>
2025-12-02 18:48:34 +0100

    Add hobbies page and update projects WIP UI
    Introduced a new HobbiesComponent with routing and navigation updates. Removed unused ProjectDetailsComponent. Enhanced the projects page with a new 'work in progress' UI and improved .gitattributes for better file handling.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import {AboutComponent} from './pages/about/about.component';
 import {ProjectsComponent} from './pages/projects/projects.component';
+import {HobbiesComponent} from './pages/hobbies/hobbies.component';
 
 export const routes: Routes = [
   { path: '', component: AboutComponent },
   { path: 'about', component: AboutComponent},
   { path: 'projects', component: ProjectsComponent},
+  { path: 'hobbies', component: HobbiesComponent},
 ];
 
